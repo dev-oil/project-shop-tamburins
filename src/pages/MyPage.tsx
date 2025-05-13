3b9e56e91b78acacda0dadce850b6ddd0dd9ccd6
devoil <devdevoildev@gmail.com>
2025-05-13 09:51:51 +0900

    feat: [#4] 불필요 인증 제거

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../lib/supabase';
 import { useUser } from '../hooks/useUser';
@@ -6,15 +5,6 @@ import { useUser } from '../hooks/useUser';
 const MyPage = () => {
   const { user } = useUser();
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (user === null) {
-      navigate('/login');
-    }
-  }, [user, navigate]);
-
-  if (user === undefined) return <p>로그인 상태 확인 중...</p>;
-  if (user === null) return null;
 
   const handleLogout = async () => {
     await supabase.auth.signOut();
@@ -26,7 +16,7 @@ const MyPage = () => {
       <section>
         <div className='p-[50px] text-center'>
           <h2>마이페이지</h2>
-          <p>{user.email}</p>
+          <p>{user?.email}</p>
           <button
             onClick={handleLogout}
             className='mt-4 bg-black text-white py-2 px-4 rounded'
